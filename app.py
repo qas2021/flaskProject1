c8a51f7fa4a01488e39b22fb8841ea728c318144
qas2021 <qasim.javid@wanclouds.com>
2021-10-11 14:34:25 +0500

    updated files

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, make_response
 from flask_bcrypt import Bcrypt
+from datetime import datetime
 from sqlalchemy import create_engine
 import re
 from flask_sqlalchemy import SQLAlchemy
@@ -9,8 +10,10 @@ import pymysql
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://qasim:Qwer_1234-@localhost/lostnfound'
+# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqldb://root:root@db:3306/jado"
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+
 
 def regex(pswd):
     if len(pswd) < 8:
@@ -19,14 +22,13 @@ def regex(pswd):
         return 'password must contain atlest one special symbol i.e !@#$%&'
     elif re.search(r'\d', pswd) is None:
         return 'password ust contain atleast one digit'
-    elif re.search('[A-Z]' , pswd) is None:
+    elif re.search('[A-Z]', pswd) is None:
         return 'password must contain one capital alphabet'
-    elif re.match(r'[a-z A-Z 0-9 !@#$%&]{8}',pswd):
+    elif re.match(r'[a-z A-Z 0-9 !@#$%&]{8}', pswd):
         return 0
 
 
-
-###Models####
+### Models ####
 class user(db.Model):
     __tablename__ = "user"
     id = db.Column(db.String(20), primary_key=True)
@@ -51,7 +53,133 @@ class user(db.Model):
         return '' % self.id
 
 
+class data(db.Model):
+    __tablename__ = "data"
+    id = db.Column(db.String(20), primary_key=True)
+    item_name = db.Column(db.String(20))
+    location = db.Column(db.String(100))
+    description = db.Column(db.String(200))
+    date = db.Column(db.String(20))
+    datai = db.Column(db.LargeBinary)
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
+
+    def __init__(self, name, loc, des, datee, dataa, id):
+        self.id = id
+        self.item_name = name
+        self.location = loc
+        self.description = des
+        self.date = datee
+        self.datai = dataa
+
+    def __repr__(self):
+        return '' % self.id
+
+
 db.create_all()
+
+
+@app.route('/search', methods=['POST'])
+def search():
+    loc = request.form['loc']
+    name = request.form['name']
+    missing = data.query.filter_by(item_name=name).filter_by(location=loc).first()
+    if missing is None:
+        return "No such item"
+    else:
+
+        # id = db.Column(db.String(20), primary_key=True)
+        # item_name = db.Column(db.String(20))
+        # location = db.Column(db.String(100))
+        # description = db.Column(db.String(200))
+        # date = db.Column(db.String(20))
+        # datai = db.Column(db.LargeBinary)
+        #
+        data_ = []
+        page = request.args.get('page', 1, type=int)
+        per_page = request.args.get('per_page', 5, type=int)
+        bookmark = data.query.filter_by(item_name=name).filter_by(location=loc).paginate(page=page, per_page=per_page, error_out= True)
+        for book in bookmark.items:
+            data_.append({
+                'id': book.id,
+                'item_name': book.item_name,
+                'location': book.location,
+                'description': book.description,
+                'date': book.date,
+                # 'img': book.datai
+            })lmjo
+        meta = {
+            'page': bookmark.page,
+            'pages': bookmark.pages,
+            'total_count': bookmark.total,
+            'prev_page': bookmark.prev_num,
+            'next_page': bookmark.next_num,
+            'has_next': bookmark.has_next,
+            'has_prev': bookmark.has_prev
+
+        }
+        return jsonify({'data': data_, 'meta': meta})
+
+
+@app.route('/update', methods=['POST'])
+def update_state():
+    loc = request.form['loc']
+    name = request.form['name']
+    idd = request.form['id']
+    img = request.files['img']
+    nloc = request.form['nloc']
+    des = request.form['description']
+    tareekh = request.form['date']
+    newname = request.form['nname']
+
+    try:
+        value = data.query.filter_by(item_name=name).filter_by(location=loc).first()
+        value.id = str(idd)
+        value.item_name = str(newname)
+        value.location = str(nloc)
+        value.description = str(des)
+        value.date = str(tareekh)
+        value.datai = img.read()
+
+        db.session.flush()
+        db.session.commit()
+        return "updated"
+    except:
+        return 'Error in def update_state'
+
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    loc = request.form['loc']
+    name = request.form['name']
+    missingg = data.query.filter_by(item_name=name).filter_by(location=loc).first()
+    if missingg is None:
+        return "No such item"
+    else:
+        data.query.filter_by(item_name=name).filter_by(location=loc).delete()
+        db.session.commit()
+        check = data.query.filter_by(item_name=name).filter_by(location=loc).first()
+        if check is None:
+            return "record deleted"
+        else:
+            return "Not deleted yet"
+
+
+@app.route('/insert', methods=["POST"])
+def insert():
+    idd = request.form['id']
+    img = request.files['img']
+    loc = request.form['loc']
+    des = request.form['description']
+    tareekh = request.form['date']
+    name = request.form['name']
+    inserted = data(name, loc, des, tareekh, img.read(), idd)
+    db.session.add(inserted)
+    db.session.commit()
+    return "data added successfully WHERE IMAGE= " + img.name
 
 
 @app.route('/')
@@ -89,7 +217,6 @@ def loginn():
     em = request.form['email']
     pswd = request.form['password']
 
-
     missingg = user.query.filter_by(email=em).first()
     if missingg is None:
         # if not bcrypt.check_password_hash(pswd_hash, pswd):
@@ -106,4 +233,4 @@ def loginn():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
